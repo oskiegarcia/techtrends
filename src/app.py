cd75d52230b426cc893badf4f2d4ad58b8386b0d
oskiegarcia <ogarc74@gmail.com>
2024-03-19 09:36:24 +1100

    add error handling and logging

--- src/app.py
+++ src/app.py
@@ -26,41 +26,66 @@ dictConfig({
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
-    connection = sqlite3.connect('database.db')
-    connection.row_factory = sqlite3.Row
-    return connection
+    try:
+        connection = sqlite3.connect('database.db')
+        connection.row_factory = sqlite3.Row
+        return connection
+    except sqlite3.Error as e:
+        app.logger.error('Error connecting to database', e)
+        return None
 
 
 # Function to get a post using its ID
 def get_post(post_id):
-    connection = get_db_connection()
-    post = connection.execute('SELECT * FROM posts WHERE id = ?',
+    try:
+        connection = get_db_connection()
+        post = connection.execute('SELECT * FROM posts WHERE id = ?',
                               (post_id,)).fetchone()
-    connection.close()
-    return post
+        connection.close()
+        return post
+    except sqlite3.Error as e:
+        app.logger.error(f'Error accessing database: {e}')
+        return None
+    except Exception as e:
+        app.logger.error(f'An expected error: {e}')
+        return None
 
 
 # Function to get the number of posts
 def get_post_count():
-    connection = get_db_connection()
-    post_count = connection.execute('SELECT count(id) FROM posts').fetchone()
-    connection.close()
-    return post_count[0]
+    try:
+        connection = get_db_connection()
+        post_count = connection.execute('SELECT count(id) FROM posts').fetchone()
+        connection.close()
+        return post_count[0]
+    except sqlite3.Error as e:
+        app.logger.error(f'Error accessing database: {e}')
+        return 0
+    except Exception as e:
+        app.logger.error(f'An expected error: {e}')
+        return 0
 
-
+# Function to get the number of active db connections
 def get_active_db_connections():
-    connection = get_db_connection()
-    cursor = connection.cursor()
+    try:
+        connection = get_db_connection()
+        cursor = connection.cursor()
 
-    # Query the master table to get the count of active connections
-    cursor.execute("PRAGMA database_list;")
-    rows = cursor.fetchall()
-    active_connections = len(rows)
+        # Query the master table to get the count of active connections
+        cursor.execute("PRAGMA database_list;")
+        rows = cursor.fetchall()
+        active_connections = len(rows)
 
-    cursor.close()
-    connection.close()
+        cursor.close()
+        connection.close()
 
-    return active_connections
+        return active_connections
+    except sqlite3.Error as e:
+        app.logger.error(f'Error accessing database: {e}')
+        return 0
+    except Exception as e:
+        app.logger.error(f'An expected error: {e}')
+        return 0
 
 
 # Define the Flask application
@@ -70,48 +95,64 @@ app.config['SECRET_KEY'] = 'your secret key'
 # Define the main route of the web application
 @app.route('/')
 def index():
-    connection = get_db_connection()
-    posts = connection.execute('SELECT * FROM posts').fetchall()
-    connection.close()
-    return render_template('index.html', posts=posts)
-
+    try:
+        connection = get_db_connection()
+        posts = connection.execute('SELECT * FROM posts').fetchall()
+        connection.close()
+        app.logger.info('Index request successfull')
+        return render_template('index.html', posts=posts)
+    except sqlite3.Error as e:
+        app.logger.error(f'Error accessing database: {e}')
+        return render_template('error.html', errorMessage="Error accessing database")
+    except Exception as e:
+        app.logger.error(f'An expected error: {e}')
+        return render_template('error.html', errorMessage="An expected error occurred")
 
 # Define how each individual article is rendered
 # If the post ID is not found a 404 page is shown
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
-    app.logger.info('Article "{}" retrieved!'.format(post['title']))
     if post is None:
+        app.logger.info('Article with id "{}" not found!'.format(post_id))
         return render_template('404.html'), 404
     else:
+        app.logger.info('Article "{}" retrieved!'.format(post['title']))
         return render_template('post.html', post=post)
 
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('About request successfull')
     return render_template('about.html')
 
 
 # Define the post creation functionality
 @app.route('/create', methods=('GET', 'POST'))
 def create():
-    print("Creating a post... ")
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
 
         if not title:
             flash('Title is required!')
+            app.logger.warn('Title is required!')
         else:
-            connection = get_db_connection()
-            connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+            try:
+                connection = get_db_connection()
+                connection.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
                                (title, content))
-            connection.commit()
-            connection.close()
-
-            return redirect(url_for('index'))
+                connection.commit()
+                connection.close()
+                app.logger.info('Create request successfull')
+                return redirect(url_for('index'))
+            except sqlite3.Error as e:
+                app.logger.error(f'Error accessing database: {e}')
+                return render_template('error.html', errorMessage="Error accessing database")
+            except Exception as e:
+                app.logger.error(f'An expected error: {e}')
+                return render_template('error.html', errorMessage="An expected error occurred")
 
     return render_template('create.html')
 
@@ -123,7 +164,7 @@ def healthcheck():
         status=200,
         mimetype='application/json'
     )
-    app.logger.info('Status request successfull')
+    app.logger.info('healthz request successfull')
     return response
 
 
